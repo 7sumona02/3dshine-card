167cd45062c204756790af77426a4b11a80820b3
7sumona02 <sumona.23548@sscbs.du.ac.in>
2025-06-19 03:04:52 +0530

    added

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,93 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
+import newStyled from "@emotion/styled";
+import Card from "./Card"
+import { animate, motion, useMotionTemplate, useMotionValue, useTransform } from "motion/react";
+import { useEffect, useRef } from "react";
+
+const RotationWrapper = newStyled(motion.div)`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transform-style: preserve-3d;
+`;
+
+const page = () => {
+  const mouseX = useMotionValue(
+    typeof window !== 'undefined' ? window.innerWidth / 2 : 0
+);
+const mouseY = useMotionValue(
+    typeof window !== 'undefined' ? window.innerHeight / 2 : 0
+);
+
+// handle mouse move on document
+useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+        // animate mouse x and y
+        animate(mouseX, e.clientX);
+        animate(mouseY, e.clientY);
+    };
+    if (typeof window === 'undefined') return;
+    // recalculate grid on resize
+    window.addEventListener('mousemove', handleMouseMove);
+    // cleanup
+    return () => {
+        window.removeEventListener('mousemove', handleMouseMove);
+    };
+}, []);
+
+const cardRef = useRef<HTMLDivElement>(null);
+
+const dampen = 40;
+const rotateX = useTransform<number, number>(mouseY, (newMouseY) => {
+    if (!cardRef.current) return 0;
+    const rect = cardRef.current.getBoundingClientRect();
+    const newRotateX = newMouseY - rect.top - rect.height / 2;
+    return -newRotateX / dampen;
+});
+const rotateY = useTransform(mouseX, (newMouseX) => {
+    if (!cardRef.current) return 0;
+    const rect = cardRef.current.getBoundingClientRect();
+    const newRotateY = newMouseX - rect.left - rect.width / 2;
+    return newRotateY / dampen;
+});
+
+const diagonalMovement = useTransform<number, number>(
+  [rotateX, rotateY],
+  ([newRotateX, newRotateY]) => {
+      const position: number = newRotateX + newRotateY;
+      return position;
+  }
+);
+const sheenPosition = useTransform(
+  diagonalMovement,
+  [-5, 5],
+  [-100, 200]
+);
+
+const sheenOpacity = useTransform(
+  sheenPosition,
+  [-100, 0, 100, 200],
+  [0, 0.15, 0.1, 0]
+);
+
+const sheenGradient = useMotionTemplate`linear-gradient(
+  55deg,
+  transparent 0%,
+  rgba(255 255 255 / ${sheenOpacity}) ${sheenPosition}%,
+  transparent)`;
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className='relative w-screen h-screen flex items-center justify-center overflow-hidden perspective-[1000px]'>
+      <RotationWrapper style={{ rotateX, rotateY }}>
+        <div className="relative z-10 h-[50vw] w-[50vw] bg-black bg-[radial-gradient(#f3f3f3,transparent_2px)] [background-size:50px_50px]"></div>        
+          <motion.div ref={cardRef} style={{ backgroundImage: sheenGradient }} className="absolute top-1/4 z-10 translate-z-40">
+            <Card />
+          </motion.div>
+      </RotationWrapper>
     </div>
-  );
+  )
 }
+
+export default page
